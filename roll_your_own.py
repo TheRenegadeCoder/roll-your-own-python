0fb548612d073740353bb247ee21b6085c80060a
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-02-19 11:39:35 -0500

    Added Power Function (#2)

--- roll_your_own.py
+++ roll_your_own.py
@@ -1,3 +1,20 @@
+def power(base, exp, mod=None):
+  '''
+  Replicates the built-in power function.
+  
+  Source: https://therenegadecoder.com/code/roll-your-own-power-function-in-python/
+  '''
+  if mod == 0:
+    raise ValueError("power() 3rd argument cannot be 0")
+  result = 1
+  for i in range(abs(exp)):
+    result *= base
+  if exp < 0:
+    result = 1 / result
+  if mod:
+    result %= mod
+  return result
+
 def upper(string):
   '''
   Replicates the upper method of strings.
